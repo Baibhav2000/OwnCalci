de4e79e60d70534d5479f1bf69da221ee00d7ea5
Baibhav Chakraborty <baibhav.chakrabortybc2000@gmail.com>
2024-02-01 17:26:49 +0530

    Added interface for NumberOps

---
+++ include/NumberOps.h
@@ -0,0 +1,12 @@
+#ifndef NUMBEROPS_H
+#define NUMBEROPS_H
+
+#include "Number.h"
+
+Number createNumber(char *);
+Number add(Number,Number);
+Number subtract(Number,Number);
+void printNumber(Number);
+
+#endif
+
